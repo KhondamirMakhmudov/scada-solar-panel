ffd4c742b9801609f9120b81129d10f4fa98781a
Khondamir Makhmudov <makhmudovkhondamir@gmail.com>
2026-05-26 10:58:14 +0500

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   JAVA_API_URL: "http://10.20.6.129:18080/",
   PYTHON_API_URL: "http://10.20.6.23:8100/api/v1/",
+  SCADA_TEST_URL: "http://10.20.6.23:8101/api/v1/",
   WEBSOCKET_URL: "ws://10.20.6.23:8102/",
-  // JAVA_API_URL: "http://10.40.9.47:8080/",
-  // PYTHON_API_URL: "http://10.40.9.46:1010/ ",
+  GENERAL_AUTH_URL: "https://app.tpp.uz",
 };
